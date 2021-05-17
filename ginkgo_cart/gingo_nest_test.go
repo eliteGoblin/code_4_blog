12b2bd3b3bbe662d9167c0c9528e5b7a13907973
Frank Sun <frank.sun@nearmap.com>
2021-05-18 07:41:31 +1000

    add fluentd

--- ginkgo_cart/gingo_nest_test.go
+++ ginkgo_cart/gingo_nest_test.go
@@ -7,11 +7,13 @@ import (
 
 var _ = Describe("Nest Test Demo", func() {
 	Context("MyTest level1", func() {
+
 		BeforeEach(func() {
 			fmt.Println("beforeEach level 1")
 		})
-		It("spec 3-1 in level1", func(){
-			fmt.Println("sepc on level 1")
+		It("spec 1-1 in level1", func(){
+			Skip("special condition wasn't met")
+			fmt.Println("sepc 1-1 on level 1")
 		})
 		Context("MyTest level2", func() {
 			BeforeEach(func() {
@@ -22,10 +24,10 @@ var _ = Describe("Nest Test Demo", func() {
 					fmt.Println("beforeEach level 3")
 				})
 				It("spec 3-1 in level3", func() {
-					fmt.Println("A simple spec in level 3")
+					fmt.Println("Spec 3-1 in level 3")
 				})
 				It("3-2 in level3", func() {
-					fmt.Println("A simple spec in level 3")
+					fmt.Println("Spec 3-2 in level 3")
 				})
 			})
 		})
